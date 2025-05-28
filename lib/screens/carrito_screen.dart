4e0e5ca10b9ea2ddf51724f46287516f58bbf1aa
Steven Huaccho <stevenhuaccho@MacBook-Pro-de-Steven.local>
2025-05-28 18:16:38 -0500

    Limpieza de codigo

--- lib/screens/carrito_screen.dart
+++ lib/screens/carrito_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/carrito_provider.dart';
-import '../models/carrito_item.dart';
+//import '../models/carrito_item.dart';
 
 class CarritoScreen extends StatelessWidget {
   const CarritoScreen({super.key});
